5e9ce5a1ade6d41b0dd977f4208b19128f1cae8b
OfuGoh <throwawayemailnothinghere@gmail.com>
2019-09-06 10:52:40 -0700

    speedy :^)

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -3,12 +3,24 @@ from test_framework import generic_test
 
 def buy_and_sell_stock_once(prices):
     # TODO - you fill in here.
-    maxSell = 0.0
+    # First attempt, takes way too much time....
+    '''maxSell = 0.0
     for i in range(len(prices)):
         for j in range(i+1, len(prices)):
             profit = prices[j] - prices[i]
             if profit > maxSell:
                 maxSell = profit
+    return maxSell'''
+    # End first attempt.
+
+    # Second attempt.
+    min = float("inf")
+    maxSell = 0.0
+    for i in range(len(prices)):
+        if prices[i] < min:
+            min = prices[i]
+        if (prices[i] - min) > maxSell:
+            maxSell = prices[i] - min
     return maxSell
 
 
